ba3ae1b69caa793c7fad1e1228cdb8b0afd48bbf
EduardoGomesdeCamargo <eduardo.camargo@sptech.com>
2025-03-19 19:38:03 -0300

    Atualizei o B.D do gp

--- BDGraosDeMetric.sql
+++ BDGraosDeMetric.sql
@@ -13,7 +13,7 @@ Vique da Silva Peixoto 078
 CREATE DATABASE DeMetric;
 USE DeMetric;
 
--- TABELA 1 CADASTRO
+-- TABELA 1 CADASTRO DA EMPRESA
 CREATE TABLE cadastro (
 	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
     nomeEmpresa VARCHAR(50),
@@ -23,8 +23,10 @@ CREATE TABLE cadastro (
     dtRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
     UFestado CHAR(2),
     cidade VARCHAR(30),
-    cep CHAR(8)
+    cep CHAR(8),
+    codeEmpresa INT
 );
+-- Inserindo dados na tabela
 INSERT INTO cadastro(nomeEmpresa, senha, cnpj, email, UFestado, cidade, cep) VALUES 
 	('Empresa1', 'minhaSenha123', '75111946000124', 'pedro.almeida@email.com', 'RS', 'Porto Alegre', '90000000'),  
 	('Empresa2', 'seguranca456', '37281060000130', 'larissa.fonseca@email.com', 'SC', 'Florianópolis', '88000000'),  
@@ -35,9 +37,27 @@ INSERT INTO cadastro(nomeEmpresa, senha, cnpj, email, UFestado, cidade, cep) VAL
 SELECT nomeEmpresa AS 'Empresa', senha AS 'Senha', cnpj AS 'CNPJ', dtRegistro AS 'Data de Registro',
  UFestado AS 'Estado', cidade AS 'Cidade', cep AS 'CEP' FROM cadastro;
  
-select * from cadastro;
+ 
+-- TABELA 2 FUNCIONÁRIO
+CREATE TABLE funcionario(
+	idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeFuncionario VARCHAR(50),
+    email VARCHAR(30),
+    senha VARCHAR(50),
+    nvlAcesso varchar(20),
+    CONSTRAINT chkNvlAcesso CHECK (nvlAcesso IN('Admin','User')),
+	codeEmpresa INT UNIQUE
+);
+-- Inserindo dados na tabela
+INSERT INTO funcionario (nomeFuncionario, email, senha, nvlAcesso) VALUES
+	('Dennis Wilson Serrano', 'dennis.wilson@gmail.com', 'lknregnj', 'Admin'),
+	('Beatriz Silva Santos', 'beatriz.silva@gmail.com', 'dsghr', 'User'),
+	('Hannah Baker', 'hannah.baker@gmail.com', '12435yu', 'User');
+    
+SELECT nomeFuncionario AS 'Nome do Funcionário', email AS 'Email', senha AS 'Senha', nvlAcesso AS 'Nivel de Acesso',
+codeEmpresa AS 'Código da empresa' FROM funcionario;
 
--- TABELA 2 DADOS DO SENSOR
+-- TABELA 3 DADOS DO SENSOR
 CREATE TABLE sensor(
 	idSensor int primary key auto_increment,
 	nomeArmazem varchar(30),
@@ -46,23 +66,22 @@ CREATE TABLE sensor(
     dataInfo datetime default current_timestamp,
     dtInstalacao datetime
 );
+-- Inserindo dados na tabela
 INSERT INTO sensor (nomeArmazem, umidade, temperatura, dtInstalacao)  
 VALUES  
 ('Armazem Norte', 45.30, 22.50, '2024-12-01 08:30:00'),  
 ('Armazem Sul', 50.75, 24.10, '2024-11-15 10:45:00'),  
 ('Armazem Leste', 38.60, 21.80, '2025-01-05 14:20:00'),  
-('Armazem Oeste', 55.20, 26.30, '2024-10-10 07:10:00'),  
+('Armazem Oeste', 55.20, 26.30, '2024-10-10 07:10:00'),
 ('Armazem Central', 42.90, 23.70, '2024-09-25 12:00:00');
-
-SELECT concat('O sensor ', idSensor, ' registrou uma umidade relativa no ar de ', umidade, '% e uma temperatura de ', temperatura,
- '°C no ', nomeArmazem, ' no dia ', date_format(dataInfo, ' %d/%m/%y %H:%m')) as 'Histórico de Registro' FROM sensor;
  
-SELECT nomeArmazem AS 'Nomde do Armazém', concat(umidade, '%') AS 'Umidade Relativa', concat(temperatura, '°C') AS 'Temperatura',
+SELECT nomeArmazem AS 'Nome do Armazém', concat(umidade, '%') AS 'Umidade Relativa', concat(temperatura, '°C') AS 'Temperatura',
  date_format(dataInfo, ' %d/%m/%Y %H:%m') AS 'Horário de Registro' FROM sensor;
-
+ 
+-- Data de instalação do sensor
 SELECT date_format(dtInstalacao, ' %d/%m/%Y') AS 'Data de Instalação' FROM sensor;
 
--- TABELA 3 PARÂMETROS DO GRÃO
+-- TABELA 4 PARÂMETROS DO GRÃO
 CREATE TABLE parametroGrao (
 	idArmazem INT PRIMARY KEY AUTO_INCREMENT,
     nomeArmazem VARCHAR(20),
@@ -70,7 +89,8 @@ CREATE TABLE parametroGrao (
     minUmidade DECIMAL(4,2),
     temperaturaMax DECIMAL(4,2),
     temperaturaMin DECIMAL(4,2)
-);  
+);
+-- Inserindo dados na tabela
 INSERT INTO parametroGrao (nomeArmazem, maxUmidade, minUmidade, temperaturaMax, temperaturaMin)  
 VALUES  
 ('Armazem Norte', 13.00, 11.00, 28.00, 12.00),
@@ -79,6 +99,6 @@ VALUES
 ('Armazem Oeste', 13.00, 11.00, 28.00, 12.00),
 ('Armazem Central', 13.00, 11.00, 28.00, 12.00);
 
-SELECT idArmazem, nomeArmazem AS 'Nome do armazem',  concat(maxUmidade, '%') AS 'Umidade maxima', 
+SELECT nomeArmazem AS 'Nome do armazem',  concat(maxUmidade, '%') AS 'Umidade maxima', 
 concat(minUmidade, '%') AS 'Umidade mínima', concat(temperaturaMax, '°C') AS 'Temperatura máxima', concat(temperaturaMin, '°C') AS 'Temperatura mínima'
 FROM parametroGrao;
